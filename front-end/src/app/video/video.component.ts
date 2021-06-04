1c7e897a674129dfa7934b1f6ec212aa33e02670
Tabea Blenk <tabeablenk@gmail.com>
2021-06-04 18:29:30 +0200

    added video

--- front-end/src/app/video/video.component.ts
+++ front-end/src/app/video/video.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-video',
-  templateUrl: './video.component.html',
-  styleUrls: ['./video.component.scss']
+  selector: "app-video",
+  templateUrl: "./video.component.html",
+  styleUrls: ["./video.component.scss"],
 })
 export class VideoComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('video') videoplayer!: ElementRef;
 
-  ngOnInit(): void {
+  constructor() {}
+
+
+  ngOnInit(): void {}
+
+  @HostListener("window:keyup", ["$event"])
+  keyEvent(event: KeyboardEvent) {
+    if (event.keyCode == 32) {
+      this.videoplayer.nativeElement.play();
+    }
   }
-
 }
